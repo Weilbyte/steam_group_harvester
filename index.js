2bcbe302942c4a796838706b1e3e4a48b6466ad7
Weilbyte <git@weilbyte.net>
2019-10-25 04:07:21 +0200

    Use requester.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const axios = require('axios');
-const parser = require('fast-xml-parser');
+const requester = require('./requester');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+console.log('[you lay eyes upon steam_group_harvester]');
+readline.question('Enter group ID: ', async (groupid) => {requester(groupid)});
