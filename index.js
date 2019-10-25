e7284fd5b2a3938cf957040bb9d9e08c4bb32ab9
Weilbyte <git@weilbyte.net>
2019-10-25 02:56:23 +0200

    Entry file

---
+++ index.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+const parser = require('fast-xml-parser');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
