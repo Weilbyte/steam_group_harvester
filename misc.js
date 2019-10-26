191a25bd957edbb2cf6047daa70b91877b4d6e0f
Weilbyte <git@weilbyte.net>
2019-10-27 00:14:49 +0200

    Result file function

--- misc.js
+++ misc.js
@@ -1,42 +1,57 @@
 const fs = require('fs');
 
+const index = require('./index');
+
 var APIKey;
-const APIKeyFile = "./api.key"
+const APIKeyFile = "./api.key";
 
 module.exports = {
-    estimateTime: function(pages, members) {
-        var seconds = (pages + (pages * 0.100) + (members * 0.010)).toFixed(1);
-        if (seconds > 59.9) {
-            var minutes = (seconds/60).toFixed(0);
-            var leftover = (seconds%60).toFixed(0);
-            if (leftover > 0) {
-                return `${minutes}m ${leftover}s`;
-            }
-            return `${minutes.toFixed(0)}m`;
-        }
-        return `${seconds}s`;
-    },
-    loadKey: function() {
-        try {
-            if (fs.existsSync(APIKeyFile)) {
-                APIKey = fs.readFileSync(APIKeyFile, 'utf8');
-                if (APIKey.length < 5) {
-                    console.log("\nPut your API key in the api.key file.");
-                    process.exit(1);
-                }
-            } else {
-                fs.writeFileSync(APIKeyFile,'KEY');
+    createResultFile: createResultFile,
+    estimateTime: estimateTime,
+    loadKey: loadKey,
+    getKey: getKey
+}
+
+async function getKey() {
+    if (APIKey != null) return APIKey;
+    console.log("\nPut your API key in the api.key file.");
+    process.exit(1);
+}
+
+function loadKey() {
+    try {
+        if (fs.existsSync(APIKeyFile)) {
+            APIKey = fs.readFileSync(APIKeyFile, 'utf8');
+            if (APIKey.length < 5) {
                 console.log("\nPut your API key in the api.key file.");
                 process.exit(1);
             }
-        } catch (error) {
-            console.log(error);
+        } else {
+            fs.writeFileSync(APIKeyFile,'KEY');
+            console.log("\nPut your API key in the api.key file.");
+            process.exit(1);
         }
-    },
-    getKey: async function() {
-        if (APIKey != null) return APIKey;
-        console.log("\nPut your API key in the api.key file.");
-        process.exit(1);
+    } catch (error) {
+        console.log(error);
     }
+}
+
+function estimateTime(pages, members) {
+    var seconds = (pages + (pages * 0.100) + (members * 0.010)).toFixed(1);
+    if (seconds > 59.9) {
+        var minutes = (seconds/60).toFixed(0);
+        var leftover = (seconds%60).toFixed(0);
+        if (leftover > 0) {
+            return `${minutes}m ${leftover}s`;
+        }
+        return `${minutes.toFixed(0)}m`;
+    }
+    return `${seconds}s`;
+}
+
+async function createResultFile(json) {
+    const ResultFile = `./profiles_${index.groupID}.json`; 
+    const stringJson = JSON.stringify(json);
+    fs.writeFileSync(ResultFile, stringJson);
 }
 
