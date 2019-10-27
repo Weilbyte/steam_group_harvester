14f06a6c82a411041428fe12dd79c83c0c5c84c7
Weilbyte <git@weilbyte.net>
2019-10-27 02:09:57 +0200

    Some error handling, plus increased delay on requests

--- chewer.js
+++ chewer.js
@@ -21,13 +21,24 @@ module.exports = {
         var i=0;
         var profileList = new Object();
         for (const steamID of SteamIDList) {
-            bar.update(++i);
-            const [username, country, state] = await chewProfile(steamID).catch(console.error);
-            profileList[steamID] = {
-                "username": username,
-                "country": country,
-                "state": state
-            };
+            try {
+                const [username, country, state] = await chewProfile(steamID).catch(console.error);
+                profileList[steamID] = {
+                    "username": username,
+                    "country": country,
+                    "state": state
+                };
+                bar.update(++i);
+            } catch (error) {
+                if (error.response.status == 500) {
+                    await new Promise(done => setTimeout(done, 1000));
+                } else if (error.response.status == 429) {
+                    await new Promise(done => setTimeout(done, 10000));
+                } else {
+                    console.log(error.response.status);
+                }
+            }
+
         }   
         await misc.createResultFile(profileList);
         bar.stop();
@@ -46,16 +57,20 @@ function getMemberElement(json) {
 }
 
 async function chewProfile(steamID) {
-    var key = await misc.getKey();
-    await new Promise(done => setTimeout(done, 10));
-    const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${steamID}`);
-    const profile = JSON.parse(JSON.stringify(response.data))['response']['players'][0];
-    var [username, country, state] = [profile['personaname'],"N/A",""];
-    if (profile['loccountrycode'] != null) {
-        country = profile['loccountrycode'];
-        if (country === 'US') {
-            if (profile['locstatecode'] != null) state = profile['locstatecode'];
+    try {
+        var key = await misc.getKey();
+        await new Promise(done => setTimeout(done, 100));
+        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${steamID}`);
+        const profile = JSON.parse(JSON.stringify(response.data))['response']['players'][0];
+        var [username, country, state] = [profile['personaname'],"N/A",""];
+        if (profile['loccountrycode'] != null) {
+            country = profile['loccountrycode'];
+            if (country === 'US') {
+                if (profile['locstatecode'] != null) state = profile['locstatecode'];
+            }
         }
+        return [username, country, state];
+    } catch (error) {
+        return error;
     }
-    return [username, country, state];
 }
