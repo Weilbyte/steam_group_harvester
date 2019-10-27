384ff687eabea8b7b23f2ccf455a5b7d79307f70
Weilbyte <git@weilbyte.net>
2019-10-27 04:26:16 +0100

    Rewritten for more efficiency

--- src/chewer.js
+++ src/chewer.js
@@ -1,48 +1,53 @@
 const convert = require('xml-js');
 const axios = require('axios');
 
+const runner = require('./runner');
 const misc = require('./misc');
 const SteamIDList = [];
 
 module.exports = {
-    xmlToJSON: function(xml) {
-        return JSON.parse(convert.xml2json(xml, {compact: false}));
-    },
-    chewPage: async function(json) {
-        const memberListElement = getMemberElement(json);
-        const memberList = json['elements']['0']['elements'][memberListElement]['elements']
-        const objMemberList = Object.keys(memberList);
-        objMemberList.forEach(function(member) {
-            let steamID = memberList[member]['elements']['0']['text'];
-            SteamIDList.push(steamID);
-        })
-    },
-    chewIDs: async function(bar) {
-        var i=0;
-        var profileList = new Object();
-        for (const steamID of SteamIDList) {
-            try {
-                const [username, country, state] = await chewProfile(steamID).catch(console.error);
-                profileList[steamID] = {
-                    "username": username,
-                    "country": country,
-                    "state": state
-                };
-                bar.update(++i);
-            } catch (error) {
-                if (error.response.status == 500) {
-                    await new Promise(done => setTimeout(done, 1000));
-                } else if (error.response.status == 429) {
-                    await new Promise(done => setTimeout(done, 10000));
-                } else {
-                    console.log(error.response.status);
-                }
-            }
+    xmlToJSON: xmlToJSON,
+    chewPage: chewPage,
+    chewIDs: chewIDs
+}
 
-        }   
-        await misc.createResultFile(profileList);
-        bar.stop();
-    }
+function xmlToJSON(xml) {
+    return JSON.parse(convert.xml2json(xml, {compact: false}));
+}
+
+async function chewPage(json) {
+    const memberListElement = getMemberElement(json);
+    const memberList = json['elements']['0']['elements'][memberListElement]['elements']
+    const objMemberList = Object.keys(memberList);
+    objMemberList.forEach(function(member) {
+        let steamID = memberList[member]['elements']['0']['text'];
+        SteamIDList.push(steamID);
+    })
+};
+
+async function chewIDs(bar) { 
+    const total = SteamIDList.length;
+    var i=0;
+    var profileList = new Object();
+    var queue = [];
+    for (const steamID of SteamIDList) {
+            bar.update(++i);
+            queue.push(steamID);
+            if ((queue.length === 100)||(i==total)) {
+                const chewedQueue = await chewProfileQueue(queue);
+                for (var chewedProfile in chewedQueue) {
+                    const steamid = chewedQueue[chewedProfile]['steamid'];
+                    profileList[steamid] = {
+                        "username": chewedQueue[chewedProfile]['username'],
+                        "country": chewedQueue[chewedProfile]['country'],
+                        "state": chewedQueue[chewedProfile]['state']
+                    }
+                }
+                queue = [];
+            }
+        }
+    await misc.createResultFile(profileList);
+    bar.stop();
 };
 
 function getMemberElement(json) {
@@ -54,23 +59,60 @@ function getMemberElement(json) {
         };
     });
     return result;
-}
+};
 
-async function chewProfile(steamID) {
+async function chewProfileQueue(profileQueue) {
     try {
-        var key = await misc.getKey();
-        await new Promise(done => setTimeout(done, 100));
-        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${steamID}`);
-        const profile = JSON.parse(JSON.stringify(response.data))['response']['players'][0];
-        var [username, country, state] = [profile['personaname'],"N/A",""];
-        if (profile['loccountrycode'] != null) {
-            country = profile['loccountrycode'];
+        const url = await buildChewProfileQueueRequestUrl(profileQueue);
+        const profiles = await sendChewProfileQueueRequest(url);
+        const chewedProfilesObject = await buildChewedProfilesObject(profiles['response']['players']);
+        return chewedProfilesObject;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+async function buildChewProfileQueueRequestUrl(profileQueue) {
+    var queueItem = 0;
+    const key = await misc.getKey();
+    var url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=`;
+    for (const steamID in profileQueue) {
+        queueItem++;
+        if (queueItem === profileQueue.length) {
+            url += `${profileQueue[steamID]}`;
+        } else {
+            url += `${profileQueue[steamID]},`;
+        }
+    }
+    return url;
+};
+
+async function buildChewedProfilesObject(profiles) {
+    var result = new Object();
+    var profileNum = 0;
+    for (var profile in profiles) {
+        const steamid = profiles[profile]['steamid'];
+        const username = profiles[profile]['personaname'];
+        var [country, state] = ["N/A",""];
+        if (profiles[profile]['loccountrycode'] != null) {
+            country = profiles[profile]['loccountrycode'];
             if (country === 'US') {
-                if (profile['locstatecode'] != null) state = profile['locstatecode'];
+                if (profiles[profile]['locstatecode'] != null) state = profiles[profile]['locstatecode'];
             }
         }
-        return [username, country, state];
-    } catch (error) {
-        return error;
+        result[profileNum++] = {
+            "steamid": steamid,
+            "username": username,
+            "country": country,
+            "state": state
+        };
     }
-}
+    return result;
+};
+
+async function sendChewProfileQueueRequest(requestUrl) {
+    await new Promise(done => setTimeout(done, 1000));
+    const response = await axios.get(requestUrl);
+    const json = JSON.parse(JSON.stringify(response.data));
+    return json;
+};
